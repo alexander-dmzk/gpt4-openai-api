50720b89f053fd760f299e4b442eaa1740d7c2ff
Alex Karyuk <hyperion342@gmail.com>
2023-11-06 23:35:22 +0300

    implemented sse streaming; code refactor; removed unnecessary code

--- server/logic.py
+++ server/logic.py
@@ -1,18 +1,15 @@
 import os
-import traceback
 
-from gpt4_openai import GPT4OpenAI
+from gpt4_openai import ChatGptDriver
 
 
 async def get_response(msg: str, model='gpt-4-browsing') -> str:
-    # Token is the __Secure-next-auth.session-token from chat.openai.com
-    try:
-        llm = GPT4OpenAI(token=os.environ["OPENAI_SESSION_TOKEN"],
-                         headless=False,
-                         model=model)
-        # GPT3.5 will answer 8, while GPT4 should be smart enough to answer 10
-        response = llm(msg)
-        return response
-    except Exception:
-        response = traceback.format_exc()
-        return response
+    chatbot = ChatGptDriver(os.environ["OPENAI_SESSION_TOKEN"],
+                            model=model)
+    return chatbot.send_message(msg)
+
+
+async def get_stream_response(msg: str, model='gpt-4-browsing'):
+    chatbot = ChatGptDriver(os.environ["OPENAI_SESSION_TOKEN"],
+                            model=model)
+    return chatbot.send_message(msg, stream=True)
--- server/routes.py
+++ server/routes.py
@@ -1,8 +1,11 @@
+import asyncio
 from typing import Literal
 
 from fastapi import APIRouter
+from sse_starlette.sse import EventSourceResponse
+from starlette.requests import Request
 
-from server.logic import get_response
+from server.logic import get_response, get_stream_response
 
 router = APIRouter(prefix='/api', tags=['main'])
 
@@ -15,3 +18,29 @@ async def get_statistic(message: str,
                             'text-davinci-002-render-sha'
                         ]):
     return await get_response(message, model)
+
+
+@router.get('/sendMessageStream')
+async def get_statistic(message: str,
+                        model: Literal[
+                            'gpt-4-browsing',
+                            'gpt-4-plugins',
+                            'text-davinci-002-render-sha'
+                        ],
+                        request: Request):
+    gen = await get_stream_response(message, model)
+
+    async def event_generator():
+        while True:
+            # If client closes connection, stop sending events
+            if await request.is_disconnected():
+                break
+
+            # Checks for new messages and return them to client if any
+            for i in gen:
+                yield i
+
+                await asyncio.sleep(0.1)
+            break
+
+    return EventSourceResponse(event_generator())
